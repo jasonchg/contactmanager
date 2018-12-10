e42eee42b2d25b9de23f3d012d77a28f1ba35c46
unknown <jason.chg@outlook.com>
2018-12-10 22:08:21 +0800

    Updates

---
+++ precache-manifest.5d8bb8d946ec4ac617d132192c313d51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "824d1fb2629cf05ec1d6",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "824d1fb2629cf05ec1d6",
+    "url": "/contactmanager/static/js/main.824d1fb2.chunk.js"
+  },
+  {
+    "revision": "943b33ca10882f8ea724",
+    "url": "/contactmanager/static/css/1.51be3ead.chunk.css"
+  },
+  {
+    "revision": "943b33ca10882f8ea724",
+    "url": "/contactmanager/static/js/1.943b33ca.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "14f1403fc3b942bf82dd79780b2600fb",
+    "url": "/contactmanager/index.html"
+  }
+];
